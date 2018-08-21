12aab006ca0ffd4e429e7686e4621f4b25fb1029
Alandryia Jones <jonesalandryia@gmail.com>
2018-08-20 20:48:12 -0500

    this is it

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,5 +2,5 @@ import { RoomComponent } from "./room/room.component";
 import { RouterModule, Routes } from "@angular/router";
 import { ModuleWithProviders } from "@angular/core";
 
-const appRoutes: Routes = [{ path: "hello", component: RoomComponent }];
+const appRoutes: Routes = [{ path: "", component: RoomComponent }];
 export const appRouting: ModuleWithProviders = RouterModule.forRoot(appRoutes);
